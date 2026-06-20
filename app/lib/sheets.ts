c0f510c34d6c7c0b1cd9a17f2a870af0e8dbde49
Joseph Centeno <centenojoseph9@gmail.com>
2026-06-19 21:21:45 -0700

    Created sheets.ts for retrieving survey data from the master sheet.

--- app/lib/sheets.ts
+++ app/lib/sheets.ts
@@ -4,11 +4,10 @@ import { google } from 'googleapis'
  * Retrieves data from a specific tab in a Google Sheet
  * @param spreadsheetId -- the ID of the master sheet
  * @param tab -- the name of the survey tab
- * @param range -- the column range to fetch survey responses
  * @returns -- the survey responses of the tab
  */
 
-export async function getSurveyData(spreadsheetId, tab, range) {
+export async function getSurveyData(spreadsheetId, tab) {
 
     // creates a Google authentication object thats scoped for only reading Google sheets
     const auth = new google.auth.GoogleAuth({
@@ -21,7 +20,7 @@ export async function getSurveyData(spreadsheetId, tab, range) {
     // creates a response that contains requested survey data from the Google sheet.
     const response = await sheets.spreadsheets.values.get({
         spreadsheetId: spreadsheetId,
-        range: `${tab}!${range}`
+        range: `${tab}!A:Z`
     })
 
     // returns row data of the request survey.
